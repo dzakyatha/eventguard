0aa3e5b6486497a1c4c657233b50814fef34f0a8
dzakyatha <18223124@std.stei.itb.ac.id>
2025-12-05 13:37:22 +0700

    feat: Update authentication method to use APIKeyHeader and add python-multipart dependency

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,14 +1,17 @@
 # skrip untuk mengatur dependensi FastAPI
 
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import APIKeyHeader
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.db.models import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = APIKeyHeader(name="Authorization")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    if token.startswith("Bearer "):
+        token = token.split(" ")[1]
+    
     user = db.query(User).filter(User.token == token).first()
     if not user:
         raise HTTPException(
